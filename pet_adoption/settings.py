db53872aabc802f98ff7e7050609df5270ad8696
istiaqfuad <istiaqurrahmanfuad@gmail.com>
2024-03-23 23:46:00 +0600

    remove and change incorrect csrf origin

--- pet_adoption/settings.py
+++ pet_adoption/settings.py
@@ -28,7 +28,7 @@ DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://petfinder.azurewebsites.net"]
 
 # Application definition
 
