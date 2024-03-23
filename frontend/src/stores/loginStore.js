23d81d17f512a681af8092056633299f96d8c862
istiaqfuad <istiaqurrahmanfuad@gmail.com>
2024-03-23 20:33:48 +0600

    seperate backend and frontend

--- frontend/src/stores/loginStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useLogin = create((set) => ({
-  loggedIn: true ? localStorage.getItem("access_token") != null : false,
-  setLoginState: (newState) => set({ loggedIn: newState }),
-}));
-
-export default useLogin;
